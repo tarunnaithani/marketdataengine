eb8cb85eca3fc403c4ee7a9b3448b8f181bbb60a
tarunnaithani <tarun.naithani@gmail.com>
2023-04-28 16:56:33 +0800

    Added test case for RandomTickPrice publication

--- test/com/marketdata/engine/TestMarketDataEngine.java
+++ test/com/marketdata/engine/TestMarketDataEngine.java
@@ -24,7 +24,27 @@ public class TestMarketDataEngine {
 
 
     @Test
-    public void TestRandomizeTickPricePublication()
+    public void TestRandomizeTickPricePublication() throws InterruptedException
+    {
+        ArrayBlockingQueue<TickData> queue = new ArrayBlockingQueue<>(1000);
+        MarketDataEngine marketDataEngine = new MarketDataEngine("TEST", 20000, queue, 10, new RandomTickPriceCalculator());
+
+        marketDataEngine.process();
+        Assert.assertEquals(10, queue.size());
+        double currentPrice = 20000;
+        for(int i =0; i< queue.size(); i++)
+        {
+            TickData data = queue.take();
+            if(i % 2 ==0)
+                Assert.assertTrue(data.getPrice() < currentPrice);
+            else
+                Assert.assertTrue(data.getPrice() > currentPrice);
+            currentPrice = data.getPrice();
+        }
+    }
+
+    @Test
+    public void TestRandomizeTickPriceCalculation()
     {
         RandomTickPriceCalculator randomTickSizeCalculator =  new RandomTickPriceCalculator();
         Assert.assertTrue(randomTickSizeCalculator.getTickPrice(0, 10) < 10);
